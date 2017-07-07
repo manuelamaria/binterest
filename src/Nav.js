a02c8fd640061c062d87e43477e60fcd67216016
manuela <manuela@MacBook-Air-Macbook.local>
2017-07-07 18:48:01 +0300

    Remove unnecessary state

--- src/Nav.js
+++ src/Nav.js
@@ -2,28 +2,11 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
 class Nav extends React.Component {
-
-  constructor(props) {
-    super(props);
-    this.state = {
-      'map' : props.map
-    }
-  }
-
-  componentWillReceiveProps(nextProps)
-  {
-    if (this.props !== nextProps) {
-      this.setState({
-        'map' : nextProps.map
-      });
-    }
-  }
-
   render() {
-    let prev = this.state.map.get(this.props.bookId)[0];
+    let prev = this.props.map.get(this.props.bookId)[0];
     let linkPrev = "/b/".concat(prev);
 
-    let next = this.state.map.get(this.props.bookId)[1];
+    let next = this.props.map.get(this.props.bookId)[1];
     let linkNext = "/b/".concat(next);
 
     return (
